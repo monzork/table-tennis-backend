10dea35381a4ca2fbd1cbbbe2bfaa11fd798492c
Defeatlol1 <jairopvvallejo@gmail.com>
2025-08-10 20:45:25 -0600

    Adding app.Post into main.go request data

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
-	"context"
+	"log"
+
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/static"
 	"github.com/joho/godotenv"
 	"github.com/monzork/table-tennis-backend/internal/db"
-	//"github.com/monzork/table-tennis-backend/internal/handlers"
-	"github.com/monzork/table-tennis-backend/internal/models"
-	"log"
 )
 
 func main() {
 	godotenv.Load()
 	db.Connect()
-	db.DB.NewInsert().Model(&models.User{Name: "Roger"}).Exec(context.Background())
 
 	app := fiber.New()
+
+	app.Post("/submit", func(c fiber.Ctx) error {
+		// Handle POST request data
+		type Players struct {
+			Name      string `json:"name"`
+			Sex       string `json:"sex"`
+			Birthdate string `json:"birthdate"`
+			Country   string `json:"country"`
+			City      string `json:"city"`
+		}
+
+		//id := uuid.New() //poner en otro lado
+
+		var p Players
+		if err := c.Bind().Body(&p); err != nil {
+			log.Println(err)
+			return c.Status(500).SendString("Failed to parse request")
+		}
+
+		return c.JSON(p) // Respond with the parsed data
+	})
 
 	app.Use("/static", static.New("./static/html/index.html"))
 	//app.Get("/users", handlers.ListUsers)
--- go.mod
+++ go.mod
@@ -31,7 +31,7 @@ require (
 	golang.org/x/crypto v0.40.0 // indirect
 	golang.org/x/exp v0.0.0-20250620022241-b7579e27df2b // indirect
 	golang.org/x/net v0.42.0 // indirect
-	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
 	modernc.org/libc v1.66.3 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
