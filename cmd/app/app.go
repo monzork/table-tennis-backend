926a03440d22fa28412cae48df2548d4dd2202f1
Defeatlol1 <jairopvvallejo@gmail.com>
2025-08-15 13:08:06 -0600

    Moving functions csrf/session.go from app.go to Infrastructure

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -1,29 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/gofiber/fiber/v3"
-	"github.com/gofiber/fiber/v3/middleware/csrf"
-	"github.com/gofiber/fiber/v3/middleware/session"
-	"github.com/gofiber/storage/sqlite3/v2"
 	"github.com/joho/godotenv"
 	"github.com/monzork/table-tennis-backend/internal/db"
 	userService "github.com/monzork/table-tennis-backend/internal/domain/user"
+	security "github.com/monzork/table-tennis-backend/internal/infrastructure/security"
+	session "github.com/monzork/table-tennis-backend/internal/infrastructure/session"
 	userRepo "github.com/monzork/table-tennis-backend/internal/infrastructure/storage"
 	user "github.com/monzork/table-tennis-backend/internal/transport/http"
 	userHanlder "github.com/monzork/table-tennis-backend/internal/transport/http/handlers"
 
 	"github.com/uptrace/bun"
 )
-
-var sqliteStorage = sqlite3.New(sqlite3.Config{
-	Database: "./sessions.db",
-	Table:    "sessions",
-})
 
 func Run() error {
 	print("test")
@@ -42,16 +34,12 @@ func Run() error {
 		AppName: "TTN",
 	})
 
+	app.Use(session.InitializeSession())
+	app.Use(security.InitializeCSRF())
+
 	api := app.Group("/api")
 
 	buildUserDependencies(api, db)
-
-	// Global middleware
-	store := initializeSession()
-	csrf := initializeCSRF()
-
-	app.Use(csrf)
-	app.Use(store)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -68,31 +56,4 @@ func buildUserDependencies(api fiber.Router, db *bun.DB) {
 	userService := userService.NewService(userRepository)
 	userHandler := userHanlder.NewUserHandler(userService)
 	user.RegisterRoutes(api, userHandler)
-}
-
-func initializeSession() fiber.Handler {
-	return session.New(session.Config{
-		Storage:         sqliteStorage,
-		CookieSecure:    true,
-		CookieHTTPOnly:  true,
-		CookieSameSite:  "lax",
-		IdleTimeout:     24 * time.Hour,
-		AbsoluteTimeout: 24 * time.Hour,
-		ErrorHandler: func(c fiber.Ctx, err error) {
-			fmt.Printf("Session error: %v", err)
-		},
-	})
-}
-
-func initializeCSRF() fiber.Handler {
-	return csrf.New(csrf.Config{
-		CookieName:        "csrf_Protection",
-		CookieSecure:      true,
-		CookieHTTPOnly:    true,
-		CookieSameSite:    "lax",
-		CookieSessionOnly: true,
-		Extractor: csrf.Chain(
-			csrf.FromHeader("X-Csrf-Token"),
-			csrf.FromForm("csrf_token")),
-	})
 }
--- internal/infrastructure/security/csrf.go
+++ internal/infrastructure/security/csrf.go
@@ -1,1 +1,19 @@
 package infrastructure
+
+import (
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/csrf"
+)
+
+func InitializeCSRF() fiber.Handler {
+	return csrf.New(csrf.Config{
+		CookieName:        "csrf_Protection",
+		CookieSecure:      true,
+		CookieHTTPOnly:    true,
+		CookieSameSite:    "lax",
+		CookieSessionOnly: true,
+		Extractor: csrf.Chain(
+			csrf.FromHeader("X-Csrf-Token"),
+			csrf.FromForm("csrf_token")),
+	})
+}
